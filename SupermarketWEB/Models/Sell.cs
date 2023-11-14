957f654573529d8b8ef2ee3104baec976fd2288e
Ajurado2321 <129556540+Ajurado2321@users.noreply.github.com>
2023-11-13 22:07:24 -0500

    Sell.cs

---
+++ SupermarketWEB/Models/Sell.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Sell
+    {
+        public int Id { get; set; }
+        public string? Date { get; set; } = default!;
+        public string? CustomerId { get; set; } = default!;
+        public string? ProductName { get; set; } = default!;
+        public int? Quantity { get; set; } = default!;
+
+        //public decimal? ProductPrice { get; set; } = default!;
+
+        [Column(TypeName = "decimal(15,2)")]
+        public decimal? ProductPrice { get; set; } = default!;
+        public decimal? TotalSale { get; set; } = default!;
+        public string? PayModeName { get; set; } = default!;
+        public string? Observation { get; set; } = default!;
+    }
+}
